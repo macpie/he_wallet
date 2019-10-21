2c907cd47b616a8ad9a355a5b2633dab66f2b476
macpie <pierre@helium.com>
2019-10-21 14:17:47 -0700

    Remove boilerplate

--- src/transactions/txn.erl
+++ src/transactions/txn.erl
@@ -2,11 +2,7 @@
 
 -export([
     create_oui_txn/4, create_oui_txn/5
-]).  
-
-%% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
+]).
 
 create_oui_txn(Owner, OwnerSigFun, Payer, Addresses) ->
     Txn = oui_txn:new(Owner, Addresses, Payer, 1, 1),
@@ -18,7 +14,3 @@ create_oui_txn(Owner, OwnerSigFun, Payer, PayerSigFun, Addresses) ->
     SignedTxn0 = oui_txn:sign(Txn, OwnerSigFun),
     SignedTxn1 = oui_txn:sign_payer(SignedTxn0, PayerSigFun),
     oui_txn:encode(SignedTxn1).
-
-%% ------------------------------------------------------------------
-%% EUNIT Tests
-%% ------------------------------------------------------------------
